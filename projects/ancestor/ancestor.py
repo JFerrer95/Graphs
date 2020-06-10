8acd660b3cc76f05a969a47579df6cf066c82a9e
JFerrer95 <jonathanferrer95@gmail.com>
2020-06-10 17:50:31 -0400

    ancestors passes DFT

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,64 @@
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+
+class Graph:
+  
+    def __init__(self):
+        self.vertices = {}
+    def add_vertex(self, vertex_id):
+        if vertex_id not in self.vertices:
+            self.vertices[vertex_id] = set()
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("You can't do that, bud.")
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    graph = Graph()
+
+
+    #Reverse the graph so starting node is at the top
+    for pair in ancestors:
+        graph.add_vertex(pair[0])
+        graph.add_vertex(pair[1])
+        graph.add_edge(pair[1], pair[0])
+
+    stack = Stack()
+    stack.push([starting_node])
+    pathMax = 1
+    ancestor = -1
+    while stack.size() > 0: 
+        path = stack.pop()
+        value = path[-1]
+        if (len(path) >= pathMax and value < ancestor) or (len(path) > pathMax):
+            ancestor = value
+            pathMax = len(path)
+        for n in graph.vertices[value]:
+            tempPath = list(path)
+            tempPath.append(n)
+            stack.push(tempPath)
+    return ancestor
